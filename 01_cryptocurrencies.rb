15ee993cfb2e850c052b61a43048ca55bd73280a
crousty24-bit <allen.kch24@gmail.com>
2026-01-16 16:37:53 +0100

    add method to update data bellow 6K currencies

--- 01_cryptocurrencies.rb
+++ 01_cryptocurrencies.rb
@@ -9,9 +9,28 @@ def cryptos_by_values(crypto_data)
   puts "Cheapest crypto: #{crypto_data.min_by {|_, price| price}.first} is #{crypto_data.min_by {|_, price| price}.last}"
 end
 
+def cryptos_bellow_6k(crypto_data)
+  count = 0
+  crypto_data_updated = []
+  crypto_data.each do |i| #i is [names, prices]
+    if i[1] < 6000.00 #compare prices
+      crypto_data_updated<< i #add in array
+      count +=1
+    end
+  end
+  crypto_data_updated.sort_by! { |_, price| -price }
+  crypto_data_updated = crypto_data_updated.take(30)
+  puts "There is #{count} cryptos currencies bellow 6K"
+  puts "This is the list :"
+  crypto_data_updated.each do |name, price|
+    puts "#{name} : #{price}"
+  end
+end
+
 def perform
   total_of_cryptos(CRYPTO_NAMES)
   cryptos_by_values(CRYPTO_DATA)
+  cryptos_bellow_6k(CRYPTO_DATA)
 end
 
 perform
